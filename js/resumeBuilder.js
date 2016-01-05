429f44de048c10c54a489e7e119b54f27c9bf77f
Dev <annablackwell44@gmail.com>
2016-01-06 11:45:58 +1300

    Added bio objects to JS file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,61 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio {
+  "name" : "Anna Blackwell",
+  "age" : 28,
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile" : "0276368721",
+    "email" : "annablackwell44@gmail.com",
+    "github" : "AnnaBlackwell",
+    "twitter" : "@annacblackwell",
+    "location" : "Auckland, New Zealand",
+},
+  "bioPic" : "../../AnnaBlackwell.github.io/ABProfile.jpg", 
+  "welcomeMessage" : "Hi, and welcome to my resume page",
+  "skills" : ["Programming", "JavaScript", "Project Management"
+  ]
+}
+
+var work {
+  "position" : "Project Manager",
+  "employer" : "Northern Regional Alliance",
+  "dates" : "2014-2016",
+  "location" : "Auckland",
+  "description" : "Managed health workforce development projects across the four DHBs in the northern region."
+}
+
+
+var education {
+  "schools" : [
+    {
+      "name" : "University of Auckland",
+      "dates" : "2008-2010",
+      "city" : "Auckland",
+      "major" : "Public Health",
+      "degree" : "Master of Public Health",
+      "url" : "https://www.fmhs.auckland.ac.nz/en/soph.html"
+    },
+    {
+      "name" : "University of Otago",
+      "dates" : "2005-2007",
+      "city" : "Dunedin",
+      "major" : "Human Nutrition",
+      "degree" : "Bachelor of Science",
+      "url" : "http://www.otago.ac.nz/sciences/study/undergraduate/qualifications/otago060422.html"
+    }
+  ]
+}
+
+var projects {
+  "projects" : [
+  {
+    "title" : "Anatomical Pathology MLT training pathway",
+    "dates" : "2014-2015",
+    "description" : "Established a training programme for new AP Medical Laboratory Technicians",
+    },
+  {
+    "title" : "Study into Older Persons' Oral Health Issues",
+    "dates" : "2012-2013",
+    "description" : "Coordinated a national survey of oral health of New Zealanders aged 65+",
+    }
+  ] 
+}
